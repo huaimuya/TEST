caadd6f4e8ab951fa748b0cb5531d9730567ae4c
yokato <2226543987@qq.com>
2022-07-20 14:07:29 +0800

    newtest

---
+++ StaticWeb/src/main/webapp/index.jsp
@@ -0,0 +1,14 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>淮木</title>
+</head>
+<body>
+<h1><%= "Hello World!" %>
+</h1>
+<h1 style="color:orangered">你好呀</h1>
+<br/>
+<a href="hello-servlet">Hello Servlet</a>
+</body>
+</html>
---
+++ src/Hello.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+/**
+ * @author huaimu
+ * @create 2022-07-19 14:20
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("args = " + Arrays.deepToString(args));
+        System.out.println("Hello.main");
+        System.out.println("args = " + args);
+        for (int i = 0; i < 100; i++) {
+
+        }
+    }
+    public void method(){
+        int i = 1;
+
+    }
+
+}
